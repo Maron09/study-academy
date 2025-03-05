94e1a2e4535ee07b74da4900aae6f5836e2458af
maron09 <chimarokeonyebi@gmail.com>
2025-03-05 11:06:10 +0100

    build

--- build.sh
+++ build.sh
@@ -3,11 +3,17 @@ pip install -r requirements.txt
 python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]]; then
-    python manage.py createsuperuser \
-        --no-input \
-        --email "$DJANGO_SUPERUSER_EMAIL" \
-        --first_name "$DJANGO_SUPERUSER_FIRSTNAME" \
-        --last_name "$DJANGO_SUPERUSER_LASTNAME"
+    python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(email="$DJANGO_SUPERUSER_EMAIL").exists():
+    User.objects.create_superuser(
+        email="$DJANGO_SUPERUSER_EMAIL",
+        password="$DJANGO_SUPERUSER_PASSWORD",
+        first_name="$DJANGO_SUPERUSER_FIRSTNAME",
+        last_name="$DJANGO_SUPERUSER_LASTNAME"
+    )
+EOF
 fi
 
 python manage.py collectstatic --noinput
