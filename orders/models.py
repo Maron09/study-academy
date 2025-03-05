fa8d9596b99e5442486177c7965acb60e29e2194
maron09 <chimarokeonyebi@gmail.com>
2025-03-05 10:49:35 +0100

    max length- order-status

--- orders/models.py
+++ orders/models.py
@@ -6,7 +6,7 @@ from courses.models import *
 class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     payment_id = models.CharField(max_length=100)
-    status = models.CharField(max_length=100)
+    status = models.CharField(max_length=20)
     amount = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     
