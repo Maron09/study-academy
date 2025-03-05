5bf0f3de86ad29039ecfd308d9a72fdb46db1283
maron09 <chimarokeonyebi@gmail.com>
2025-03-05 09:52:00 +0100

    Hosts

--- study_academy/settings.py
+++ study_academy/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["study_academy.onrender.com"]
+ALLOWED_HOSTS = ["study-academy.onrender.com"]
 
 
 # Application definition
